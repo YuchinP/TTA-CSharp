6a39c7b0b47d01c2e19e4287c825711368be5c30
Yuchin Pan <yuchinspan@gmail.com>
2016-11-26 16:12:41 -0800

    Fitness Frog Treehouse

---
+++ Program1.5/Program1.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Greet the User with the entered name
+            Console.WriteLine("Hello, " + name);
+
+            //Wait for the User to acknowledge
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
---
+++ Program1/Program1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program1
+{
+    public class Program
+    {
+        //This is where the program starts.
+        static void Main(string[] args)
+        {
+            //Ask user to enter a name.
+            Console.WriteLine("Enter your name, please:");
+
+            //Now read the name entered
+            string name = Console.ReadLine();
+
+            //Greet the User with the entered name
+            Console.WriteLine("Hello, " + name);
+
+            //Wait for the User to acknowledge
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
