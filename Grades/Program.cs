e9bf3e667de61870de8f395f59335949f44491ec
YuchinP <yuchinspan@gmail.com>
2016-12-02 16:33:06 -0800

    PluralSight C# Update

---
+++ Grades/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Yay");
+
+            GradeBook book = new GradeBook(); /*Invoked the Gradebook class so we can use it's modules*/
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+        }
+    }
+}
